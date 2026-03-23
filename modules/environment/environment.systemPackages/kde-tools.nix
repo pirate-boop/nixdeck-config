00bd58dd7457ee2d6c1ca79e6406ebc26631aff1
Kori <daniilignatev290@proton.me>
2026-03-24 02:36:23 +0300

    Add kdePackages.kate and kdePackages.dolphin
    Added kdePackages.kate and kdePackages.dolphin to the list of system packages.

--- modules/environment/environment.systemPackages/kde-tools.nix
+++ modules/environment/environment.systemPackages/kde-tools.nix
@@ -1,9 +1,12 @@
 with pkgs; [
-  kdePackages.ark           # File archiver by KDE
-  # kdePackages.okular      # KDE document viewer
-  # kdePackages.gwenview    # Image viewer by KDE
-  kdePackages.kcalc         # Calculator offering everything a scientific calculator does, and more
-  # kdePackages.kcm_flatpak # НЕ СУЩЕСТВУЕТ В NIXPKGS
-  # kdePackages.flatpak-kcm   # Flatpak Permissions Management KCM
-  # kdePackages.discover      # Helps you find and install applications, games, and tools
+  kdePackages.ark                # File archiver by KDE
+  # kdePackages.okular           # KDE document viewer
+  # kdePackages.gwenview         # Image viewer by KDE
+  kdePackages.kcalc              # Calculator offering everything a scientific calculator does, and more
+  # kdePackages.kcm_flatpak      # НЕ СУЩЕСТВУЕТ В NIXPKGS
+  # kdePackages.flatpak-kcm      # Flatpak Permissions Management KCM
+  # kdePackages.discover         # Helps you find and install applications, games, and tools
+  kdePackages.kate
+  # kdePackages.dolphin          # File manager by KDE
+  # kdePackages.dolphin-plugins  # Plugins for Dolphin
 ]
