bc49bbb05d71d5482bdd271457a64c183cf0716f
Kori <daniilignatev290@proton.me>
2026-03-23 19:28:12 +0300

    Update configuration.nix to modify imports
    Comment out the system_apps module and add environment package paths.

--- configuration.nix
+++ configuration.nix
@@ -7,12 +7,15 @@
   imports = [
     ./hardware-configuration.nix   # Железо (генерится системой)
     ./modules/system-base.nix      # Сеть, локали, юзер, ядро
-    ./modules/system_apps.nix      # Софт, браузеры, терминалы
+    # ./modules/system_apps.nix      # Софт, браузеры, терминалы
     ./modules/jovian-deck.nix      # Steam, Gamescope, OLED фиксы
     ./modules/decky.nix            # Decky Loader, PowerTools, права
     ./modules/aliases.nix          # Handbook и сокращения
     ./modules/waydroid.nix         # Waydroid
     ./modules/niri-custom.nix      # Noctalia сервис
+
+    ./modules/environment/environment.systemPackages/HEADsystemPackages.nix
+    ./modules/environment/environment.systemPackages/systemPackages.nix
     # home.nix подключается через flake.nix (home-manager.users.decky)
   ];
 }
