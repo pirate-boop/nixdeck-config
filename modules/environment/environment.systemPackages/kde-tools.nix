c344ddaa65aabecbc86797e0910faa2ac580f9ba
Kori <daniilignatev290@proton.me>
2026-03-24 02:30:49 +0300

    Update kde-tools.nix to include additional KDE packages
    Added several KDE tools to the system packages list.

--- modules/environment/environment.systemPackages/kde-tools.nix
+++ modules/environment/environment.systemPackages/kde-tools.nix
@@ -1,1 +1,9 @@
-
+with pkgs; [
+  kdePackages.ark
+  # kdePackages.okular      # KDE document viewer
+  # kdePackages.gwenview    # Image viewer by KDE
+  kdePackages.kcalc
+  # kdePackages.kcm_flatpak # НЕ СУЩЕСТВУЕТ В NIXPKGS
+  kdePackages.flatpak-kcm   # Flatpak Permissions Management KCM
+  kdePackages.discover      # Helps you find and install applications, games, and tools
+]
