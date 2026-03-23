f69bedbce88c19c8ef65ffac4965b4d8ab22f7fa
Kori <daniilignatev290@proton.me>
2026-03-24 02:32:45 +0300

    Refactor comments in kde-tools.nix
    Updated comments for kde-tools package list.

--- modules/environment/environment.systemPackages/kde-tools.nix
+++ modules/environment/environment.systemPackages/kde-tools.nix
@@ -1,9 +1,9 @@
 with pkgs; [
-  kdePackages.ark
+  kdePackages.ark           # File archiver by KDE
   # kdePackages.okular      # KDE document viewer
   # kdePackages.gwenview    # Image viewer by KDE
-  kdePackages.kcalc
+  kdePackages.kcalc         # Calculator offering everything a scientific calculator does, and more
   # kdePackages.kcm_flatpak # НЕ СУЩЕСТВУЕТ В NIXPKGS
-  kdePackages.flatpak-kcm   # Flatpak Permissions Management KCM
-  kdePackages.discover      # Helps you find and install applications, games, and tools
+  # kdePackages.flatpak-kcm   # Flatpak Permissions Management KCM
+  # kdePackages.discover      # Helps you find and install applications, games, and tools
 ]
