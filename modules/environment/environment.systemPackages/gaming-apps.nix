3c8f77cf92b41cd1823b6b4efb117df309e3b7cc
Kori <daniilignatev290@proton.me>
2026-03-24 02:59:06 +0300

    Enhance comments in gaming-apps.nix
    Updated comments for clarity on gaming applications.

--- modules/environment/environment.systemPackages/gaming-apps.nix
+++ modules/environment/environment.systemPackages/gaming-apps.nix
@@ -1,7 +1,7 @@
 with pkgs; [
-  # steam   # Digital distribution platform
-  lutris    # Open Source gaming platform for GNU/Linux
-  heroic
-  mangohud
-  protonup-qt
+  # steam     # Digital distribution platform
+  lutris      # Open Source gaming platform for GNU/Linux
+  heroic      # Native GOG, Epic, and Amazon Games Launcher for Linux, Windows and Mac
+  mangohud    # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more
+  protonup-qt # Install and manage Proton-GE and Luxtorpeda for Steam and Wine-GE for Lutris with this graphical user interface
 ]
