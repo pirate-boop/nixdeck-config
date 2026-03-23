54f115d59edb0b00ab2d94e9542e6012e2e338ef
Kori <daniilignatev290@proton.me>
2026-03-23 19:22:33 +0300

    Update systemPackages.nix

--- modules/environment/environment.systemPackages/systemPackages.nix
+++ modules/environment/environment.systemPackages/systemPackages.nix
@@ -1,1 +1,11 @@
-
+environment.systemPackages =
+  decky-deps ++
+  terminal-apps ++
+  browser-apps ++
+  social-apps ++
+  media-apps ++
+  kde-tools ++
+  gaming-apps ++
+  virt-tools ++
+  system-tools ++
+  system-DE;
