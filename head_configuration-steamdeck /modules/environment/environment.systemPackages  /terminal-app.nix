02f0ce4e97cbcfaf6150524274ae70625c853af6
Kori <daniilignatev290@proton.me>
2026-03-24 06:52:46 +0300

    Create terminal-app.nix

---
+++ head_configuration-steamdeck /modules/environment/environment.systemPackages  /terminal-app.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+with pkgs; [
+  wget
+  git
+  htop
+  fastfetch
+  mission-center
+  # --- Терминалы ---
+  vscodium
+  # ghostty
+  # alacritty
+  # kitty
+  wezterm
+  # foot
+  tilix
+  terminator
+]
