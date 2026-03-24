87ebd4d073b136f9e6cb086f8b4aed30531d340a
Kori <daniilignatev290@proton.me>
2026-03-24 09:01:31 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -14,9 +14,7 @@
     ./modules/waydroid.nix         # Waydroid
     ./modules/niri-custom.nix      # Noctalia сервис
 
-    ./head_configuration-steamdeck/modules/environment/environment.variables/defaultApplications/==defaultApplications==.nix # Приложения по умолчанию
-    ./modules/environment/environment.systemPackages/==systemPackages==.nix                                                  # Софт, браузеры, терминалы 
-    ./modules/environment/environment.variables/aliases/==aliases==.nix                                                      # Handbook и сокращения
+    
     
     
     
