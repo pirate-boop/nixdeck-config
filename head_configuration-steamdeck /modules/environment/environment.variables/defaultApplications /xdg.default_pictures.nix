3233f5574cfc2107a03d15d3c5cac609a96469f2
Kori <daniilignatev290@proton.me>
2026-03-24 08:52:42 +0300

    Update xdg.default_pictures.nix

--- head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_pictures.nix
+++ head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_pictures.nix
@@ -1,1 +1,19 @@
+{ config, pkgs, ... }:
 
+{
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = {
+    # Растровые форматы
+    "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+    "image/png" = [ "org.gnome.Loupe.desktop" ];
+    "image/gif" = [ "org.gnome.Loupe.desktop" ];
+    "image/webp" = [ "org.gnome.Loupe.desktop" ];
+    "image/bmp" = [ "org.gnome.Loupe.desktop" ];
+    "image/tiff" = [ "org.gnome.Loupe.desktop" ];
+    
+    # Современные и специфические форматы
+    "image/heif" = [ "org.gnome.Loupe.desktop" ];
+    "image/avif" = [ "org.gnome.Loupe.desktop" ];
+    "image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
+  };
+}
