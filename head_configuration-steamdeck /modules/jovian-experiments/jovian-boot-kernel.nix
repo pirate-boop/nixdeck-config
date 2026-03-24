12a953748754192d834ec4d6d4800128b8052378
Kori <daniilignatev290@proton.me>
2026-03-24 11:17:28 +0300

    Update jovian-boot-kernel.nix

--- head_configuration-steamdeck /modules/jovian-experiments/jovian-boot-kernel.nix
+++ head_configuration-steamdeck /modules/jovian-experiments/jovian-boot-kernel.nix
@@ -6,13 +6,20 @@
 { pkgs, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  # Специальное ядро Jovian с патчами от Valve
-  boot.kernelPackages = pkgs.linuxPackages_jovian; 
-  
-  # Твик для Inotify (чтобы игры не вылетали при нехватке лимитов на файлы)
+  boot.kernelPackages = pkgs.linuxPackages_jovian;
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
-  
-  # Фикс отвала Wi-Fi на OLED при пробуждении
+
+  # Фикс отвала Qualcomm ath11k при переходе в сон
   powerManagement.enable = true;
+  powerManagement.powerDownCommands = "${pkgs.kmod}/bin/modprobe -r ath11k_pci || true";
+  powerManagement.resumeCommands = "${pkgs.kmod}/bin/modprobe ath11k_pci || true";
+
+  # JOVIAN HARDWARE SUPPORT
+  jovian.devices.steamdeck.enable = true;
+  jovian.devices.steamdeck.enableVendorDrivers = true;
+
+  # AMD GPU Твики
+  jovian.hardware.has.amd.gpu = true;
+  jovian.hardware.amd.gpu.enableBacklightControl = true;
+  jovian.hardware.amd.gpu.enableEarlyModesetting = true;
 }
