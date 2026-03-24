b5137ff9b910948e54c2755b57c52659448e673a
Kori <daniilignatev290@proton.me>
2026-03-24 07:29:13 +0300

    Create networking.nix

---
+++ head_configuration-steamdeck /modules/system_core/networking.nix
@@ -0,0 +1,12 @@
+{ ... }: {
+  networking.hostName = "NIXDECK";
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+  networking.hosts = { "198.18.4.40" = [ "steamdeckrepo.com" ]; };
+  networking.networkmanager.settings = {
+    connectivity.enabled = false;
+    connection."ipv6.method" = "ignore";
+  };
+  services.resolved.enable = true;
+  networking.firewall.enable = false;
+}
