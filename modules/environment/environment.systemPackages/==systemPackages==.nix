c16c51113d40fef8a4e35cbd77ed1e21106f5e80
Kori <daniilignatev290@proton.me>
2026-03-24 04:46:59 +0300

    Update ==systemPackages==.nix

--- modules/environment/environment.systemPackages/==systemPackages==.nix
+++ modules/environment/environment.systemPackages/==systemPackages==.nix
@@ -1,27 +1,27 @@
 { pkgs, inputs, ... }:
 let
-  # Импортируем списки прямо тут, иначе environment.systemPackages их не увидит
-  decky-deps     = import ./decky-deps.nix     { inherit pkgs; };
-  terminal-apps  = import ./terminals.nix      { inherit pkgs; };
-  browser-apps   = import ./browsers.nix       { inherit pkgs; };
-  social-apps    = import ./social.nix         { inherit pkgs; };
-  media-apps     = import ./media.nix          { inherit pkgs; };
-  kde-tools      = import ./kde-tools.nix      { inherit pkgs; };
-  gaming-apps    = import ./gaming.nix         { inherit pkgs; };
-  virt-tools     = import ./virt.nix           { inherit pkgs; };
-  system-tools   = import ./system-tools.nix   { inherit pkgs; };
-  system-DE      = import ./system-DE.nix      { inherit pkgs; };
+  # ВАЖНО: Пути должны в точности совпадать с именами файлов в папке environment.systemPackages
+  decky-deps    = import ./decky-deps.nix    { inherit pkgs inputs; };
+  terminal-apps = import ./terminal-app.nix  { inherit pkgs inputs; };
+  browser-apps  = import ./browser-apps.nix  { inherit pkgs inputs; };
+  social-apps   = import ./social-apps.nix   { inherit pkgs inputs; };
+  media-apps    = import ./media-apps.nix    { inherit pkgs inputs; };
+  kde-tools     = import ./kde-tools.nix     { inherit pkgs inputs; };
+  gaming-apps   = import ./gaming-apps.nix   { inherit pkgs inputs; };
+  virt-tools    = import ./virt-tools.nix    { inherit pkgs inputs; };
+  system-tools  = import ./system-tools.nix  { inherit pkgs inputs; };
+  system-DE     = import ./system-DE.nix     { inherit pkgs inputs; };
 in
 {
-  environment.systemPackages =
-    decky-deps ++
-    terminal-apps ++
-    browser-apps ++
-    social-apps ++
-    media-apps ++
-    kde-tools ++
-    gaming-apps ++
-    virt-tools ++
-    system-tools ++
+  environment.systemPackages = 
+    decky-deps ++ 
+    terminal-apps ++ 
+    browser-apps ++ 
+    social-apps ++ 
+    media-apps ++ 
+    kde-tools ++ 
+    gaming-apps ++ 
+    virt-tools ++ 
+    system-tools ++ 
     system-DE;
 }
