6e0649667f1d5d7e5cdc8cfe9c2f245ad322f06c
Kori <daniilignatev290@proton.me>
2026-03-24 10:32:06 +0300

    Update 1_jovian-boot-kernel.nix

--- head_configuration-steamdeck /modules/jovian-experiments/1_jovian-boot-kernel.nix
+++ head_configuration-steamdeck /modules/jovian-experiments/1_jovian-boot-kernel.nix
@@ -1,1 +1,13 @@
-
+{ pkgs, ... }: {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Специальное ядро Jovian с патчами от Valve
+  boot.kernelPackages = pkgs.linuxPackages_jovian; 
+  
+  # Твик для Inotify (чтобы игры не вылетали при нехватке лимитов на файлы)
+  boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
+  
+  # Фикс отвала Wi-Fi на OLED при пробуждении
+  powerManagement.enable = true;
+}
