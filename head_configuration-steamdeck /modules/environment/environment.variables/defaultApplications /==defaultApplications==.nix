f7ac7b4c9d3201d0122912b90f8386e2b9d4ce0b
Kori <daniilignatev290@proton.me>
2026-03-24 09:02:29 +0300

    Update ==defaultApplications==.nix

--- head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /==defaultApplications==.nix
+++ head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /==defaultApplications==.nix
@@ -1,1 +1,12 @@
+{ config, pkgs, ... }:
 
+{
+  imports = [
+    ./xdg.default_archive.nix      # Ark для архивов [cite: 47]
+    ./xdg.default_file_explorer.nix # Thunar для папок [cite: 51]
+    ./xdg.default_pictures.nix      # Loupe для фото 
+    ./xdg.default_text-editor.nix   # Kate для текста 
+    ./xdg.default_video.nix         # VLC для видео 
+    ./xdg.default_browser.nix       # Пустой шаблон под Opera
+  ];
+}
