68215cc02966689016abea8dfe6cbc56c794e62a
Kori <daniilignatev290@proton.me>
2026-03-24 08:51:33 +0300

    Update xdg.default_video.nix

--- head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_video.nix
+++ head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_video.nix
@@ -1,1 +1,19 @@
+{ config, pkgs, ... }:
 
+{
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = {
+    # Основные видео-контейнеры
+    "video/mp4" = [ "vlc.desktop" ];
+    "video/x-matroska" = [ "vlc.desktop" ];
+    "video/webm" = [ "vlc.desktop" ];
+    "video/x-flv" = [ "vlc.desktop" ];
+    "video/quicktime" = [ "vlc.desktop" ]; # .mov файлы
+    
+    # Стриминговые и прочие форматы
+    "video/mpeg" = [ "vlc.desktop" ];
+    "video/ogg" = [ "vlc.desktop" ];
+    "video/x-msvideo" = [ "vlc.desktop" ]; # .avi файлы
+    "video/x-flic" = [ "vlc.desktop" ];
+  };
+}
