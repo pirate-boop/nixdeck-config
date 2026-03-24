e36112ab75acda02cf79979fd6aceb93224dce15
Kori <daniilignatev290@proton.me>
2026-03-24 06:50:29 +0300

    Create kde-tools.nix

---
+++ head_configuration-steamdeck /modules/environment/environment.systemPackages  /kde-tools.nix
@@ -0,0 +1,13 @@
+{ pkgs, inputs, ... }:
+with pkgs; [
+  kdePackages.ark                # File archiver by KDE
+  # kdePackages.okular           # KDE document viewer
+  # kdePackages.gwenview         # Image viewer by KDE
+  kdePackages.kcalc              # Calculator offering everything a scientific calculator does, and more
+  # kdePackages.kcm_flatpak      # НЕ СУЩЕСТВУЕТ В NIXPKGS
+  # kdePackages.flatpak-kcm      # Flatpak Permissions Management KCM
+  # kdePackages.discover         # Helps you find and install applications, games, and tools
+  kdePackages.kate               # Advanced text editor
+  # kdePackages.dolphin          # File manager by KDE
+  # kdePackages.dolphin-plugins  # Plugins for Dolphin
+]
