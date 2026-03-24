67a52dc6e396470a5d2f23a1f13d1b9c452d2ea5
Kori <daniilignatev290@proton.me>
2026-03-24 09:01:59 +0300

    Update ==head==.nix

--- head_configuration-steamdeck /==head==.nix
+++ head_configuration-steamdeck /==head==.nix
@@ -1,1 +1,3 @@
-
+    ./head_configuration-steamdeck/modules/environment/environment.variables/defaultApplications/==defaultApplications==.nix # Приложения по умолчанию
+    ./modules/environment/environment.systemPackages/==systemPackages==.nix                                                  # Софт, браузеры, терминалы 
+    ./modules/environment/environment.variables/aliases/==aliases==.nix                                                      # Handbook и сокращения
