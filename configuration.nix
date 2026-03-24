cf6267ae1e60aa077765b6b28d7e1fa58c5822bb
Kori <daniilignatev290@proton.me>
2026-03-24 05:14:39 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -7,10 +7,10 @@
   imports = [
     ./hardware-configuration.nix   # Железо (генерится системой)
     ./modules/system-base.nix      # Сеть, локали, юзер, ядро
-    # ./modules/system_apps.nix      # Софт, браузеры, терминалы
+    
     ./modules/jovian-deck.nix      # Steam, Gamescope, OLED фиксы
     ./modules/decky.nix            # Decky Loader, PowerTools, права
-    ./modules/aliases.nix          # Handbook и сокращения
+    
     ./modules/waydroid.nix         # Waydroid
     ./modules/niri-custom.nix      # Noctalia сервис
 
@@ -19,6 +19,10 @@
     #./modules/environment/environment.systemPackages/HEADsystemPackages.nix
     #./modules/environment/environment.systemPackages/systemPackages.nix
     # home.nix подключается через flake.nix (home-manager.users.decky)
+
+    # исключенные из выбора
+    # ./modules/system_apps.nix      # Софт, браузеры, терминалы
+    # ./modules/aliases.nix          # Handbook и сокращения
   ];
 }
 #****************************************************************#
