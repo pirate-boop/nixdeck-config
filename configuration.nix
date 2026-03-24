d606c9159c74ba035109005913553d7d30e9e173
Kori <daniilignatev290@proton.me>
2026-03-24 09:00:39 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,16 +6,17 @@
 {
   imports = [
     ./hardware-configuration.nix   # Железо (генерится системой)
-    ./modules/system-base.nix      # Сеть, локали, юзер, ядро
+    
     
     ./modules/jovian-deck.nix      # Steam, Gamescope, OLED фиксы
     ./modules/decky.nix            # Decky Loader, PowerTools, права
     
     ./modules/waydroid.nix         # Waydroid
     ./modules/niri-custom.nix      # Noctalia сервис
-    
-    ./modules/environment/environment.systemPackages/==systemPackages==.nix    # Софт, браузеры, терминалы 
-    ./modules/environment/environment.variables/aliases/==aliases==.nix        # Handbook и сокращения
+
+    ./head_configuration-steamdeck/modules/environment/environment.variables/defaultApplications/==defaultApplications==.nix # Приложения по умолчанию
+    ./modules/environment/environment.systemPackages/==systemPackages==.nix                                                  # Софт, браузеры, терминалы 
+    ./modules/environment/environment.variables/aliases/==aliases==.nix                                                      # Handbook и сокращения
     
     
     
@@ -23,6 +24,7 @@
     # исключенные из выбора
     # ./modules/system_apps.nix      # Софт, браузеры, терминалы
     # ./modules/aliases.nix          # Handbook и сокращения
+    # ./modules/system-base.nix      # Сеть, локали, юзер, ядро
     # ./home.nix подключается через flake.nix (home-manager.users.kori)
 
     # --- EMERGENCY BACKUP SECTION (Keep commented) ---
