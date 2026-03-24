d8793d848fd70eb11adab52833cf06eec0bf05f7
Kori <daniilignatev290@proton.me>
2026-03-24 06:02:47 +0300

    Update backup(old-configuration)-01.nix

--- modules/backups/old-configuration01/backup(old-configuration)-01.nix
+++ modules/backups/old-configuration01/backup(old-configuration)-01.nix
@@ -1,1 +1,10 @@
+{ config, pkgs, inputs, ... }:
 
+{
+  imports = [
+    # Пути ведут в папку nixdeck-config/modules, которая лежит рядом
+    ./nixdeck-config/modules/system_apps.nix
+    ./nixdeck-config/modules/aliases.nix
+    # Добавь сюда остальные старые файлы из той папки, если они нужны
+  ];
+}
