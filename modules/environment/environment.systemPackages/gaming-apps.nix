58c7cfb47bbafab969ad3e515490c771f675e486
Kori <daniilignatev290@proton.me>
2026-03-24 03:02:59 +0300

    Update gaming-apps.nix

--- modules/environment/environment.systemPackages/gaming-apps.nix
+++ modules/environment/environment.systemPackages/gaming-apps.nix
@@ -1,8 +1,9 @@
 with pkgs; [
-  # steam     # Digital distribution platform
-  lutris      # Open Source gaming platform for GNU/Linux
-  heroic      # Native GOG, Epic, and Amazon Games Launcher for Linux, Windows and Mac
-  mangohud    # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more
-  protonup-qt # Install and manage Proton-GE and Luxtorpeda for Steam and Wine-GE for Lutris with this graphical user interface
-  bottles     # Easy-to-use wineprefix manager
+  # steam                  # Digital distribution platform
+  lutris                   # Open Source gaming platform for GNU/Linux
+  heroic                   # Native GOG, Epic, and Amazon Games Launcher for Linux, Windows and Mac
+  mangohud                 # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more
+  protonup-qt              # Install and manage Proton-GE and Luxtorpeda for Steam and Wine-GE for Lutris with this graphical user interface
+  bottles                  # Easy-to-use wineprefix manager
+  wineWow64Packages.stable # Open Source implementation of the Windows API on top of X, OpenGL, and Unix
 ]
