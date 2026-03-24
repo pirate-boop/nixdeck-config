26ffb4ea6bb6541ab9a958c263dc86c383d26783
Kori <daniilignatev290@proton.me>
2026-03-24 08:54:08 +0300

    Update xdg.default_archive.nix

--- head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_archive.nix
+++ head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_archive.nix
@@ -1,1 +1,20 @@
+{ config, pkgs, ... }:
 
+{
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = {
+    # Самые ходовые архивы
+    "application/zip" = [ "org.kde.ark.desktop" ];
+    "application/x-7z-compressed" = [ "org.kde.ark.desktop" ];
+    "application/x-rar" = [ "org.kde.ark.desktop" ];
+    "application/x-tar" = [ "org.kde.ark.desktop" ];
+    
+    # Сжатые Linux-архивы
+    "application/x-gzip" = [ "org.kde.ark.desktop" ];
+    "application/x-bzip2" = [ "org.kde.ark.desktop" ];
+    "application/x-xz" = [ "org.kde.ark.desktop" ];
+    
+    # Образы и прочее
+    "application/x-iso9660-image" = [ "org.kde.ark.desktop" ];
+  };
+}
