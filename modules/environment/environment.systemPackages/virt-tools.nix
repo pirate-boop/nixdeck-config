818d389fcbc43dd2bec438451259a1bbc0a7c28d
Kori <daniilignatev290@proton.me>
2026-03-24 04:39:32 +0300

    Update virt-tools.nix

--- modules/environment/environment.systemPackages/virt-tools.nix
+++ modules/environment/environment.systemPackages/virt-tools.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 with pkgs; [
-  virt-manager
-  distrobox
-  qemu
+  virt-manager # Desktop user interface for managing virtual machines
+  distrobox    # Wrapper around podman or docker to create and start containers
+  qemu         # Generic and open source machine emulator and virtualizer
 ]
