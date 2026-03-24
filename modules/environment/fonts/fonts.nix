b7930de6d2df8adaa1939fe192ccbb4677326aeb
Kori <daniilignatev290@proton.me>
2026-03-24 06:12:29 +0300

    Update fonts.nix

--- modules/environment/fonts/fonts.nix
+++ modules/environment/fonts/fonts.nix
@@ -1,1 +1,12 @@
+{ pkgs, ... }:
 
+{
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    liberation_ttf
+    (nerd-fonts.fira-code)
+    (nerd-fonts.jetbrains-mono)
+  ];
+}
