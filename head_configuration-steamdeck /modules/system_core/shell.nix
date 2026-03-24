654ed48bce3dffff4ab25d525548e9ca635e9270
Kori <daniilignatev290@proton.me>
2026-03-24 07:47:13 +0300

    Create shell.nix

---
+++ head_configuration-steamdeck /modules/system_core/shell.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  programs.zsh = {
+    enable = true; # 
+    enableCompletion = true; # 
+    autosuggestions.enable = true; # 
+    syntaxHighlighting.enable = true; # 
+    ohMyZsh = { 
+      enable = true; 
+      theme = "robbyrussell"; 
+    }; # 
+  };
+}
