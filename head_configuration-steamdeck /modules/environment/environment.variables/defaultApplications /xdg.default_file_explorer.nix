9c84c7e6fd64ec6a004a68391270743207a41ed9
Kori <daniilignatev290@proton.me>
2026-03-24 08:49:56 +0300

    Update xdg.default_file_explorer.nix

--- head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_file_explorer.nix
+++ head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_file_explorer.nix
@@ -1,1 +1,13 @@
+{ config, pkgs, ... }:
 
+{
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = {
+    # Основной запуск папок
+    "inode/directory" = [ "thunar.desktop" ];
+    
+    # Обработка поиска и специфических путей
+    "x-scheme-handler/file" = [ "thunar.desktop" ];
+    "x-scheme-handler/trash" = [ "thunar.desktop" ];
+  };
+}
