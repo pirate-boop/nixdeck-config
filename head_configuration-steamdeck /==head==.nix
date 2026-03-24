2832f8814782cf5ee92872a75686ef2ab8f35b93
Kori <daniilignatev290@proton.me>
2026-03-24 09:40:18 +0300

    Update ==head==.nix

--- head_configuration-steamdeck /==head==.nix
+++ head_configuration-steamdeck /==head==.nix
@@ -1,3 +1,16 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    # Твои новые системные модули (networking, users и т.д.)
+    # ./modules/core/networking.nix
+    # ...
+
+    # Блок ассоциаций приложений (XDG)
     ./head_configuration-steamdeck/modules/environment/environment.variables/defaultApplications/==defaultApplications==.nix # Приложения по умолчанию
+
+    # Остальной софт и настройки
     ./modules/environment/environment.systemPackages/==systemPackages==.nix                                                  # Софт, браузеры, терминалы 
     ./modules/environment/environment.variables/aliases/==aliases==.nix                                                      # Handbook и сокращения
+  ];
+}
