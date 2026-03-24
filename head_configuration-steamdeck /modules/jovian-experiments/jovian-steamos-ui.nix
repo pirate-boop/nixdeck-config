e0214707f2a0649c0bbf4ba5d033f0619f4d5a53
Kori <daniilignatev290@proton.me>
2026-03-24 12:22:16 +0300

    Update jovian-steamos-ui.nix

--- head_configuration-steamdeck /modules/jovian-experiments/jovian-steamos-ui.nix
+++ head_configuration-steamdeck /modules/jovian-experiments/jovian-steamos-ui.nix
@@ -1,16 +1,16 @@
 { ... }: {
   jovian.steam = {
     enable = true;
-    autoLoginUser = "kori";
+    autoStart = true;
     user = "kori";
-    desktopSession = "niri"; # Твой основной десктоп
-  };
+    desktopSession = "niri";
 
-  # Сетевые фиксы (чтобы Steam не висел на проверке соединения)
-  environment.variables = {
-    STEAM_CONNECTIVITY_CHECK_DISABLED = "1";
-    DISABLE_STEAM_WIFI_CHECK = "1";
-    STEAM_IS_ONLINE_IF_ANY_INTERFACE_UP = "1";
-    CEF_REMOTE_DEBUGGING = "1";
+    # Сетевые фиксы должны быть ВНУТРИ блока jovian.steam
+    environment = {
+      STEAM_CONNECTIVITY_CHECK_DISABLED = "1";
+      DISABLE_STEAM_WIFI_CHECK = "1";
+      STEAM_IS_ONLINE_IF_ANY_INTERFACE_UP = "1";
+      CEF_REMOTE_DEBUGGING = "1";
+    };
   };
 }
