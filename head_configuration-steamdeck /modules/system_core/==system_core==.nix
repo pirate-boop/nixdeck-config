7eb16181f2615a5a0c175df6686b6eb2ba26f06e
Kori <daniilignatev290@proton.me>
2026-03-24 09:47:28 +0300

    Update ==system_core==.nix

--- head_configuration-steamdeck /modules/system_core/==system_core==.nix
+++ head_configuration-steamdeck /modules/system_core/==system_core==.nix
@@ -1,1 +1,14 @@
+{ config, pkgs, inputs, ... }:
 
+{
+  imports = [
+    ./networking.nix        # Хостнейм и сеть 
+    ./localization.nix      # Часовой пояс и локали
+    ./nixos-settings.nix    # Флейки, NUR и stateVersion
+    ./hardware-services.nix # Звук, графика, ввод
+    ./users.nix             # Юзер kori и группы
+    ./shell.nix             # Конфиг ZSH
+    ./programs.nix          # Steam и Niri
+    ./virtualization.nix    # Libvirtd
+  ];
+}
