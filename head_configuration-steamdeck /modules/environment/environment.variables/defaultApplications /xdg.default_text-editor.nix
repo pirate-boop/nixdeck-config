00134f0d11e79d68acdacdc0c5c1ca42a1f1457e
Kori <daniilignatev290@proton.me>
2026-03-24 08:45:28 +0300

    Update xdg.default_text-editor.nix

--- head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_text-editor.nix
+++ head_configuration-steamdeck /modules/environment/environment.variables/defaultApplications /xdg.default_text-editor.nix
@@ -1,1 +1,19 @@
+{ config, pkgs, ... }:
 
+{
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = {
+    # Текстовые файлы и логи
+    "text/plain" = [ "org.kde.kate.desktop" ];
+    "text/markdown" = [ "org.kde.kate.desktop" ];
+    
+    # Конфиги и код (чтобы не ждать запуска Codium ради одной строки)
+    "text/x-nix" = [ "org.kde.kate.desktop" ];
+    "text/x-shellscript" = [ "org.kde.kate.desktop" ];
+    "application/x-yaml" = [ "org.kde.kate.desktop" ];
+    "application/json" = [ "org.kde.kate.desktop" ];
+    
+    # Системные и пустые файлы
+    "application/x-zerosize" = [ "org.kde.kate.desktop" ];
+  };
+}
